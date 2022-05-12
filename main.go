f2eb30694aa3337a610c4e327ffe6f73a2c64125
gabriel-eb <gabo_espburg@hotmail.com>
2022-05-11 20:15:32 -0500

    Add read and map DB funcs, add comments

--- main.go
+++ main.go
@@ -1,36 +1,61 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 )
 
+// Add the database path
 const DB string = "./data.csv"
 
+// Create pokemon struct
 type Pokemon struct {
 	Id string `json:"id"`
 	Name string `json:"name"`
 }
 
+// Global array for pokemons
 var Pokemons []Pokemon
 
-func readDB(){
+// readDB reads a CSV file and return the matrix of entries
+func readDB() [][]string{
 	log.Println("INFO - Reading DB")
-	// TODO: read from csv file
-	Pokemons = []Pokemon{
-		{Id: "1", Name: "bulbasaur"},
-		{Id: "2", Name: "ivasaur"},
+
+	var file, err = os.OpenFile(DB, os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	data, _ := reader.ReadAll();
+	return data
+}
+
+// mapDBtoStructArr takes a matrix of strings (the result from DB)
+// and map it to the Struct Array
+func mapDBtoStructArr(data [][]string){
+	for _, d := range data {
+		Pokemons = append(Pokemons, Pokemon{
+			Id: d[0],
+			Name: d[1],
+		})
 	}
 }
 
+// index return the index page content
 func index(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	log.Println("INFO - Home Page")
-	readDB()
+	data := readDB()
+	mapDBtoStructArr(data)
 	json.NewEncoder(w).Encode(Pokemons)
 }
 
+// startServer starts the server
 func startServer(){
 	http.HandleFunc("/", index)
 	log.Fatal(http.ListenAndServe(":3030", nil))
