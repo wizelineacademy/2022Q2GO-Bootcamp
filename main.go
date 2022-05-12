25bfa6da02d36ed85a086315c227472d65d55d46
Jesus Revilla <jrevilla@Jesuss-MacBook-Air.local>
2022-05-11 21:46:26 -0500

    Return slice of data structs

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type data struct {
-	Index string
+	Index int64
 	Item  string
 }
 
-func readFile(fileName string) [][]string {
+func readFile(fileName string) []data {
+	items := make([]data, 0)
+
 	csvFile, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
@@ -24,7 +27,20 @@ func readFile(fileName string) [][]string {
 		fmt.Println()
 	}
 
-	return csvLines
+	for _, line := range csvLines {
+		index, err := strconv.ParseInt(line[0], 10, 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		item := data{
+			Index: index,
+			Item:  line[1],
+		}
+		items = append(items, item)
+	}
+
+	return items
 }
 
 func main() {
