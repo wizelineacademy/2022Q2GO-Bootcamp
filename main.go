5ed868be99e81bbf50d09285bf31c89b52d5f02b
Guillermo Tinoco <m3mo89@gmail.com>
2022-05-13 20:08:10 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func readCsv(pokemonId string) ([]string, error) {
+	fileName := "pokemon.csv"
+
+	fs, err := os.Open(fileName)
+
+	if err != nil {
+		return nil, fmt.Errorf("can not open the file, err is %+v", err)
+	}
+
+	defer fs.Close()
+
+	reader := csv.NewReader(fs)
+
+	for {
+		content, err := reader.Read()
+
+		if err != nil && err != io.EOF {
+			return nil, fmt.Errorf("can not read, err is %+v", err)
+		}
+		if err == io.EOF {
+			break
+		}
+
+		if content[0] == pokemonId {
+			return content, nil
+		}
+	}
+
+	return nil, fmt.Errorf("Not found, err is %+v", err)
+}
+
+func handler(response http.ResponseWriter, request *http.Request) {
+	keys, ok := request.URL.Query()["id"]
+
+	if !ok || len(keys[0]) < 1 {
+		log.Fatal("Url Param 'id' is missing")
+		return
+	}
+
+	key := keys[0]
+
+	content, errCsv := readCsv(key)
+
+	if errCsv != nil {
+		log.Fatal(fmt.Errorf("can not read the csv file: %v", errCsv))
+	}
+
+	jsonEncoder := json.NewEncoder(response)
+	jsonEncoder.SetIndent("", "  ")
+
+	err := jsonEncoder.Encode(content)
+
+	if err != nil {
+		log.Fatal(fmt.Errorf("impossible to encode pokemons: %v", err))
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("localhost:8080", nil)
+}
