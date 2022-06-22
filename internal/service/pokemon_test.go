95d20f87e0e279ed708ea5dbbe5e8ebf31cb94ec
krmirandas <kevinmiranda29@ciencias.unam.mx>
2022-06-22 01:38:50 -0500

    Create mocks and fix unit test

--- internal/service/pokemon_test.go
+++ internal/service/pokemon_test.go
@@ -2,117 +2,197 @@ package service
 
 import (
 	"errors"
-	"strconv"
 	"testing"
 
 	"github.com/krmirandas/2022Q2GO-Bootcamp/internal/entity"
+	"github.com/krmirandas/2022Q2GO-Bootcamp/internal/service/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
 var errCRUD = errors.New("error crud")
 
-type mockRepository struct {
-	repo PokemonRepo
+func TestService_FindPokemonById(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		filter   string
+		response entity.Pokemon
+		err      error
+		// Repository
+		repoRes entity.Pokemon
+		repoErr error
+	}{
+		{
+			"Should return a pokemon",
+			"8",
+			entity.Pokemon{
+				ID:         "700",
+				Name:       "Sylveon",
+				Type1:      "Fairy",
+				Type2:      "",
+				Total:      "525",
+				HP:         "95",
+				Attack:     "65",
+				Defense:    "65",
+				SpAtk:      "110",
+				SpDef:      "130",
+				Speed:      "60",
+				Generation: "6",
+				Legendary:  "False",
+			},
+			nil,
+			entity.Pokemon{
+				ID:         "700",
+				Name:       "Sylveon",
+				Type1:      "Fairy",
+				Type2:      "",
+				Total:      "525",
+				HP:         "95",
+				Attack:     "65",
+				Defense:    "65",
+				SpAtk:      "110",
+				SpDef:      "130",
+				Speed:      "60",
+				Generation: "6",
+				Legendary:  "False",
+			},
+			nil,
+		},
+		{
+			"Should return a pokemon",
+			"8",
+			entity.Pokemon{},
+			errors.New("error find"),
+			entity.Pokemon{},
+			errors.New("error find"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// testObj := new(.mockPokemonRepo)
+			testObj := mocks.NewPokemonRepository(t)
+
+			// setup expectations
+			testObj.On("ReadOnePokemon", tc.filter).Return(tc.repoRes, tc.repoErr)
+
+			// call the code we are testing
+			s := NewPokemonService(testObj)
+
+			res, err := s.FindPokemonById(tc.filter)
+
+			assert.Equal(t, tc.response, res)
+			assert.Equal(t, tc.err, err)
+		})
+	}
 }
 
-func Test_service(t *testing.T) {
-	s := NewPokemonService(mockRepository{})
-	pokemonMock := entity.Pokemon{
-		ID:         "700",
-		Name:       "Sylveon",
-		Type1:      "Fairy",
-		Type2:      "",
-		Total:      "525",
-		HP:         "95",
-		Attack:     "65",
-		Defense:    "65",
-		SpAtk:      "110",
-		SpDef:      "130",
-		Speed:      "60",
-		Generation: "6",
-		Legendary:  "False",
+func TestService_FindPokemon(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		response []entity.Pokemon
+		err      error
+		// Repository
+		repoRes []entity.Pokemon
+		repoErr error
+	}{
+		{
+			"Should return array of pokemons",
+			[]entity.Pokemon{
+				{
+					ID:         "700",
+					Name:       "Sylveon",
+					Type1:      "Fairy",
+					Type2:      "",
+					Total:      "525",
+					HP:         "95",
+					Attack:     "65",
+					Defense:    "65",
+					SpAtk:      "110",
+					SpDef:      "130",
+					Speed:      "60",
+					Generation: "6",
+					Legendary:  "False",
+				},
+			},
+			nil,
+			[]entity.Pokemon{
+				{
+					ID:         "700",
+					Name:       "Sylveon",
+					Type1:      "Fairy",
+					Type2:      "",
+					Total:      "525",
+					HP:         "95",
+					Attack:     "65",
+					Defense:    "65",
+					SpAtk:      "110",
+					SpDef:      "130",
+					Speed:      "60",
+					Generation: "6",
+					Legendary:  "False",
+				},
+			},
+			nil,
+		},
+		{
+			"Should throw errors",
+			[]entity.Pokemon(nil),
+			errCRUD,
+			[]entity.Pokemon(nil),
+			errCRUD,
+		},
 	}
-	id := pokemonMock.ID
 
-	// get
-	_, err := s.FindPokemonById("none")
-	assert.NotNil(t, err)
-	pokemon, err := s.FindPokemonById(id)
-	assert.Nil(t, err)
-	assert.Equal(t, pokemonMock.Name, pokemon.Name)
-	assert.Equal(t, id, pokemon.ID)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			testObj := mocks.NewPokemonRepository(t)
 
-	// fiind
-	_, err = s.FindPokemon()
-	assert.NotNil(t, err)
+			// setup expectations
+			testObj.On("ReadPokemon").Return(tc.repoRes, tc.repoErr)
 
-	// count
-	count, _ := s.Count()
-	assert.Equal(t, 1, count)
+			// call the code we are testing
+			s := NewPokemonService(testObj)
+
+			res, err := s.FindPokemon()
+
+			assert.Equal(t, tc.response, res)
+			assert.Equal(t, tc.err, err)
+		})
+	}
 }
 
-func (m mockRepository) Count() (int, error) {
-	var pokemons []entity.Pokemon
-	pokemonMock := entity.Pokemon{
-		ID:         "700",
-		Name:       "Sylveon",
-		Type1:      "Fairy",
-		Type2:      "",
-		Total:      "525",
-		HP:         "95",
-		Attack:     "65",
-		Defense:    "65",
-		SpAtk:      "110",
-		SpDef:      "130",
-		Speed:      "60",
-		Generation: "6",
-		Legendary:  "False",
+func TestService_Count(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		response int
+		err      error
+		// Repository
+		repoRes int
+		repoErr error
+	}{
+		{
+			"Should return a count",
+			1,
+			nil,
+			1,
+			nil,
+		},
 	}
-	pokemons = append(pokemons, pokemonMock)
-	return len(pokemons), nil
-}
 
-func (m mockRepository) ReadOnePokemon(id string) (entity.Pokemon, error) {
-	var pokemon entity.Pokemon
-	_, err := strconv.Atoi(id)
-	if err != nil {
-		return pokemon, err
-	}
-	pokemonMock := entity.Pokemon{
-		ID:         "700",
-		Name:       "Sylveon",
-		Type1:      "Fairy",
-		Type2:      "",
-		Total:      "525",
-		HP:         "95",
-		Attack:     "65",
-		Defense:    "65",
-		SpAtk:      "110",
-		SpDef:      "130",
-		Speed:      "60",
-		Generation: "6",
-		Legendary:  "False",
-	}
-	return pokemonMock, err
-}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			testObj := mocks.NewPokemonRepository(t)
 
-func (m mockRepository) ReadPokemon() ([]entity.Pokemon, error) {
-	var pokemons []entity.Pokemon
-	pokemonMock := entity.Pokemon{
-		ID:         "700",
-		Name:       "Sylveon",
-		Type1:      "Fairy",
-		Type2:      "",
-		Total:      "525",
-		HP:         "95",
-		Attack:     "65",
-		Defense:    "65",
-		SpAtk:      "110",
-		SpDef:      "130",
-		Speed:      "60",
-		Generation: "6",
-		Legendary:  "False",
+			// setup expectations
+			testObj.On("Count").Return(tc.repoRes, tc.repoErr)
+
+			// call the code we are testing
+			s := NewPokemonService(testObj)
+
+			res, err := s.Count()
+
+			assert.Equal(t, tc.response, res)
+			assert.Equal(t, tc.err, err)
+		})
 	}
-	pokemons = append(pokemons, pokemonMock)
-	return pokemons, errCRUD
 }
